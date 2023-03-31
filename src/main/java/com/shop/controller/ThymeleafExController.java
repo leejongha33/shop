b0f24545560c49f2be7fe3ae9790443118ac0d4e
leejongha <qmffn040@gmail.com>
2023-03-31 15:14:19 +0900

    Thymeleaf 적용 및 사용2

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,9 +1,12 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -16,4 +19,16 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx01";
     }
 
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
+    }
 }

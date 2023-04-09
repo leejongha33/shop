fb8ef6ca43932ef8a65083391aee77464e1cfbbc
leejongha <qmffn040@gmail.com>
2023-04-09 18:18:30 +0900

    Thymeleaf 페이지 레이아웃 세팅

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -88,4 +88,10 @@ public class ThymeleafExController {
 
         return "thymeleafEx/thymeleafEx06";
     }
+
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07() {
+
+        return "thymeleafEx/thymeleafEx07";
+    }
 }

653edf6c174c6cc5e241ec2622c4835f6f681ddd
leejongha <qmffn040@gmail.com>
2023-04-14 23:13:38 +0900

    회원 정보 디렉토리 생성

---
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -0,0 +1,10 @@
+package com.shop.dto;
+
+public class MemberFormDto {
+
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+    private int phoneNumber;
+}
---
+++ src/main/java/com/shop/entity/Member.java
@@ -0,0 +1,36 @@
+package com.shop.entity;
+
+import com.shop.constant.Role;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private int phoneNumber;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+}
